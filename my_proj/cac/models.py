3bbfa131478cbc0a7df447e3553fe0b568fd6584
Eugenio <lugezz@gmail.com>
2022-11-01 11:16:34 -0300

    Clase 21

--- my_proj/cac/models.py
+++ my_proj/cac/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    apellido = models.CharField(max_length=150, verbose_name='Apellido')
+    email = models.EmailField(max_length=150)
+    dni = models.IntegerField(verbose_name="DNI")
